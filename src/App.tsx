435d306f38d9b6f3ce6764fff77052342d9fd346
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-04-21 17:46:01 -0300

    Página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import SingIn from './pages/SingIn';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <SingIn />
       <GlobalStyle />
     </>
   );
